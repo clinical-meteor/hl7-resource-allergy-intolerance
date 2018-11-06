23d20689f50adfdfe567d58f6f3cd3d0842b1055
Abigail Watson <abigailwatson@Abigails-Mac-mini-380428.local>
2018-11-06 11:57:25 -0600

    fhir safety checklist

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-allergy-intolerance',
-  version: '1.3.9',
+  version: '1.3.10',
   summary: 'HL7 FHIR Resource - AllergyIntolerance',
   git: 'https://github.com/clinical-meteor/hl7-resource-allergy-intolerance',
   documentation: 'README.md'
