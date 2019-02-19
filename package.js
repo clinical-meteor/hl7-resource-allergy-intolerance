4746ba936f5e51a59e745133b29fc394cd629f41
Abigail Watson <awatson1978@gmail.com>
2019-02-19 01:37:59 -0600

    sync and publish

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-allergy-intolerance',
-  version: '1.3.12',
+  version: '1.3.13',
   summary: 'HL7 FHIR Resource - AllergyIntolerance',
   git: 'https://github.com/clinical-meteor/hl7-resource-allergy-intolerance',
   documentation: 'README.md'
