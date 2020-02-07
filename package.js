03e0b260dd5d52819e0e54a98fbee6d121fc22b4
Abigail Watson <awatson1978@gmail.com>
2020-02-06 20:25:03 -0600

    resolving dependencies and binary build

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-allergy-intolerance',
-  version: '1.5.2',
+  version: '1.5.3',
   summary: 'HL7 FHIR Resource - AllergyIntolerance',
   git: 'https://github.com/clinical-meteor/hl7-resource-allergy-intolerance',
   documentation: 'README.md'
